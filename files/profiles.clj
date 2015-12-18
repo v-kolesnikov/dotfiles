1327d78e5fe1ca798a47f747fbfb24a01ff7f7cb
V.Kolesnikov <re.vkolesnikov@gmail.com>
2015-12-18 17:24:38 +0300

    lein: add lein-vanity plugin

--- files/profiles.clj
+++ files/profiles.clj
@@ -1,6 +1,7 @@
 {:user  {:plugins  [[venantius/ultra "0.4.0"]
                     [lein-kibit "0.1.2"]
                     [lein-cljfmt "0.3.0"]
+                    [lein-vanity "0.2.0"]
                     [lein-cloverage "1.0.6"]
                     [jonase/eastwood "0.2.2"]]
          :dependencies [[org.clojure/clojure "1.7.0"]
